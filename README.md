cbca048301ca96c0732394cff38dc06ece204fe4
Keivry <keivry@163.com>
2025-04-03 10:47:33 +0800

    ChangeLogs:     1. 删除部分代码     2. 修复 flatpak 打包

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
+<!--toc:start-->
+
 # com.alibabainc.dingtalk-flatpak
+
 钉钉 flatpak
 
 参考[微信打包](https://github.com/web1n/wechat-universal-flatpak)
-#  自行编译
+
+# 自行编译
+
 ```
 flatpak-builder --user --install --force-clean build-dir com.alibabainc.DingTalk.yaml
 ```
 
 # 打开
+
 ```
 flatpak run com.alibabainc.DingTalk
 ```
 
 # libc6-amd64
+
 ```
     依赖glibc，该包随时可能更新，如果无法build报错，可以去 https://mirrors.ustc.edu.cn/debian/pool/main/g/glibc/ 下载对应的 libc6-amd64 包
     需要更改文件下载的URL和sha256sum
--- dingtalk.sh
+++ dingtalk.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+export QT_QPA_PLATFORM=xcb
+export QT_PLUGIN_PATH=/app/extra/dingtalk/current_version
+
 setup_ime_env() {
-  if [[ "$XMODIFIERS" =~ fcitx ]]; then
-    [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=fcitx
-    [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=fcitx
-  elif [[ "$XMODIFIERS" =~ ibus ]]; then
-    [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
-    [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
-  fi
+    if [[ "$XMODIFIERS" =~ fcitx ]]; then
+        [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=fcitx
+        [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=fcitx
+    elif [[ "$XMODIFIERS" =~ ibus ]]; then
+        [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
+        [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
+    fi
 }
 
 setup_ime_env
 
 ulimit -c 0
 
-exec /app/bin/Elevator.sh "$@"
+cd /app/extra/dingtalk/current_version || exit 1
+
+env LD_PRELOAD="/app/lib/glibc/lib64/libm.so.6 /app/lib/libstdc/libstdc++.so.6" \
+    ./com.alibabainc.dingtalk "$@"
